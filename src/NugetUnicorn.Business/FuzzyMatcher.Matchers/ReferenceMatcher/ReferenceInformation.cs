014ea67df076ec743467381679fea531547d9a95
dmitry.shilonosov <dshilonosov@dataart.com>
2017-05-22 10:25:52 +0200

    fixed an issue when assembly was loaded from NugetUnicord context rather than from file path

--- src/NugetUnicorn.Business/FuzzyMatcher.Matchers/ReferenceMatcher/ReferenceInformation.cs
+++ src/NugetUnicorn.Business/FuzzyMatcher.Matchers/ReferenceMatcher/ReferenceInformation.cs
@@ -24,7 +24,7 @@ namespace NugetUnicorn.Business.FuzzyMatcher.Matchers.ReferenceMatcher
                 throw new ArgumentException($"Reference file could not be found in specified location: {fullPath}", nameof(fullPath));
             }
 
-            var assembly = Assembly.LoadFile(fullPath);
+            var assembly = Assembly.ReflectionOnlyLoadFrom(fullPath);
             var assemblyName = assembly.GetName();
 
             AssemblyName = assemblyName.Name;
